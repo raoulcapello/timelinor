c14036dc57c7171c0dec8b4b39723f0320cbd9f0
Raoul Capello <72148841+raoulcapello@users.noreply.github.com>
2021-07-16 09:23:42 +0200

    Tweak local dev environment
    - Add django_extensions to local dev INSTALLED apps
    - Create local.py settings file

---
+++ timelinor/settings/local.py
@@ -0,0 +1,8 @@
+"""
+Local Development Settings
+"""
+
+# If using in your own project, update the project namespace below
+from timelinor.settings.base import *
+
+INSTALLED_APPS.append('django_extensions')
