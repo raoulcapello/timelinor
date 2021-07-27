376e6019f362a6d2ca71dcf0bb5c2f53407e629f
Raoul Capello <72148841+raoulcapello@users.noreply.github.com>
2021-07-27 15:08:53 +0200

    Add slug field to timeline model

--- timelines/models.py
+++ timelines/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 from accounts.models import User
 
@@ -7,6 +8,14 @@ class Timeline(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=500)
+    slug = models.SlugField(max_length=100, unique=True, null=True)
+
+    # Based on:
+    # https://kodnito.com/posts/slugify-urls-django/
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
